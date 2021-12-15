b9457bfbdcbbb97072da6e6e98fae9266b4d7aa9
Cyborck <fredrik.borck@gmail.com>
2021-12-15 15:24:07 +0100

    (ToT)/~~~

--- src/com/cyborck/chaosGame/ChaosGame.java
+++ src/com/cyborck/chaosGame/ChaosGame.java
@@ -1,41 +1,120 @@
 package com.cyborck.chaosGame;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ChaosGame {
     private final Point[] corners;
-    private final List<Point> points;
+    private final double factor;
+    private final Restriction r;
 
-    public ChaosGame ( int width, int height, Point... corners ) {
+    private Point last;
+    private int lastCornerIndex;
+    private int penultimateCornerIndex;
+
+    public ChaosGame ( int width, int height, double factor, Restriction r, Point... corners ) {
         this.corners = corners;
+        this.factor = factor;
+        this.r = r;
 
-        points = new ArrayList<>();
-        points.add( Point.random( width, height ) );
+        last = Point.random( width, height );
+        lastCornerIndex = 0;
+        penultimateCornerIndex = 0;
     }
 
-    public ChaosGame ( int width, int height, int startPointCount ) {
+    public ChaosGame ( int width, int height, double factor, Restriction r, int startPointCount ) {
         // generate random start points
         corners = new Point[ startPointCount ];
         for ( int i = 0; i < startPointCount; i++ )
             corners[ i ] = Point.random( width, height );
+        this.factor = factor;
+        this.r = r;
 
-        points = new ArrayList<>();
-        points.add( Point.random( width, height ) );
+        last = Point.random( width, height );
+        lastCornerIndex = 0;
+        penultimateCornerIndex = 0;
+    }
+
+    public static ChaosGame pointsAroundCircle ( int width, int height, double factor, Restriction r, int startPointCount, double radius ) {
+        double offsetX = width / 2d;
+        double offsetY = height / 2d;
+
+        Point[] points = new Point[ startPointCount ];
+
+        double alpha = -Math.PI / 2;
+        double alphaIncrease = 2 * Math.PI / startPointCount;
+        for ( int i = 0; i < startPointCount; i++, alpha += alphaIncrease ) {
+            double x = offsetX + Math.cos( alpha ) * radius;
+            double y = offsetY + Math.sin( alpha ) * radius;
+            points[ i ] = new Point( x, y );
+        }
+
+        return new ChaosGame( width, height, factor, r, points );
     }
 
     public Point next () {
-        int r = ( int ) ( Math.random() * corners.length );
+        int cornerIndex = -1;
 
-        Point last = points.get( points.size() - 1 );
-        Point corner = corners[ r ];
+        switch ( r ) {
+            case R0 -> cornerIndex = ( int ) ( Math.random() * corners.length );
+            case R1 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( cornerIndex == lastCornerIndex );
+            }
+            case R2 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( cornerIndex == lastCornerIndex - 1 || ( lastCornerIndex == 0 && cornerIndex == corners.length - 1 ) );
+            }
+            case R3 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( cornerIndex == lastCornerIndex - 1 || cornerIndex == lastCornerIndex + 1 || ( lastCornerIndex == 0 && cornerIndex == corners.length - 1 )
+                        || ( lastCornerIndex == corners.length - 1 && cornerIndex == 0 ) );
+            }
+            case R4 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( cornerIndex == lastCornerIndex - 2 || ( lastCornerIndex == 0 && cornerIndex == corners.length - 2 ) || ( lastCornerIndex == 1 && cornerIndex == corners.length - 1 ) );
+            }
+            case R5 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( cornerIndex == lastCornerIndex - 2 || cornerIndex == lastCornerIndex + 2 || ( lastCornerIndex == 0 && cornerIndex == corners.length - 2 )
+                        || ( lastCornerIndex == 1 && cornerIndex == corners.length - 1 ) || ( lastCornerIndex == corners.length - 2 && cornerIndex == 0 )
+                        || ( lastCornerIndex == corners.length - 1 && cornerIndex == 1 ) );
+            }
+            case R6 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( lastCornerIndex == penultimateCornerIndex && cornerIndex == lastCornerIndex );
+            }
+            case R7 -> {
+                do {
+                    cornerIndex = ( int ) ( Math.random() * corners.length );
+                } while ( lastCornerIndex == penultimateCornerIndex && ( cornerIndex == lastCornerIndex - 1 || cornerIndex == lastCornerIndex + 1 || ( lastCornerIndex == 0 && cornerIndex == corners.length - 1 )
+                        || ( lastCornerIndex == corners.length - 1 && cornerIndex == 0 ) ) );
+            }
+        }
+        Point corner = corners[ cornerIndex ];
 
-        double centerX = ( last.getX() + corner.getX() ) / 2;
-        double centerY = ( last.getY() + corner.getY() ) / 2;
+        double centerX = last.getX() + ( corner.getX() - last.getX() ) * factor;
+        double centerY = last.getY() + ( corner.getY() - last.getY() ) * factor;
         Point center = new Point( centerX, centerY );
 
-        points.add( center );
+        last = center;
+        penultimateCornerIndex = lastCornerIndex;
+        lastCornerIndex = cornerIndex;
 
         return center;
+    }
+
+    public enum Restriction {
+        R0, // no restrictions
+        R1, // not allowed to use the previous corner again
+        R2, // corner cannot be one place (anti-clockwise) away from previous corner
+        R3, // corner cannot be one place (both directions) away from previous corner
+        R4, // corner cannot be two places (anti-clockwise) away from previous corner
+        R5, // corner cannot be two places (both directions) away from previous corner
+        R6, // corner cannot be the same as the previous chosen corner if the two previously chosen corners are the same
+        R7, // corner cannot neighbour the previous chosen corner if the two previously chosen corners are the same
     }
 }
--- src/com/cyborck/chaosGame/Main.java
+++ src/com/cyborck/chaosGame/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main ( String[] args ) {
         System.setProperty( "sun.java2d.uiScale", "1.0" );
 
-        int width = 1000;
-        int height = 1000;
-        int repetitions = 100000000;
+        int width = 2000;
+        int height = 2000;
+        int repetitions = 500000000;
 
-        chaosGame = new ChaosGame( width, height, 3 );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R7, 5, width / 2d );
         gui = new Gui( width, height );
 
         run( repetitions );
@@ -22,5 +22,37 @@ public class Main {
             Point next = chaosGame.next();
             gui.addPoint( next );
         }
+    }
+
+    private static void configurations () {
+        int width = 2000;
+        int height = 2000;
+
+        // list of cool looking configurations
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R0, 3, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .6, ChaosGame.Restriction.R0, 8, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .51, ChaosGame.Restriction.R0, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, 1.5, ChaosGame.Restriction.R0, 3, width / 7.5 );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, 1.6, ChaosGame.Restriction.R0, 3, width / 7.5 );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, 1.6, ChaosGame.Restriction.R0, 4, width / 10d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R1, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R1, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R1, 10, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R2, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R2, 3, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R2, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .6, ChaosGame.Restriction.R2, 10, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R3, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R3, 10, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R4, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .4, ChaosGame.Restriction.R5, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R5, 8, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .6, ChaosGame.Restriction.R5, 8, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .7, ChaosGame.Restriction.R5, 20, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R6, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R6, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R7, 4, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R7, 5, width / 2d );
+        chaosGame = ChaosGame.pointsAroundCircle( width, height, .5, ChaosGame.Restriction.R7, 6, width / 2d );
     }
 }
